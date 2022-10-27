26c9267a2805460fc8026579df3be404e0ed6382
Aleksandr <36007931+7StaSH7@users.noreply.github.com>
2022-10-27 15:46:20 +0300

    lab4/done

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -4,20 +4,30 @@ import numpy as np
 
 
 def main() -> None:
-    spring_stiffness_koef = 40  # Н/м
+    spring_stiffness_koef = 0.9
 
-    vent_weigth = 1  # кг
-    vent_radius = 0.01  # м
+    vent_weigth = 1
+    vent_radius = 5
 
-    vent_oscillation_period = 2
     spring_oscillation_period = 2 * np.pi * np.sqrt(vent_weigth / spring_stiffness_koef)
+    vent_oscillation_period = 5 * spring_oscillation_period
 
-    vent_freq =  np.pi / vent_oscillation_period
+    print(
+        "Отношение периода вращения вентилятора к периоду колебания пружины:",
+        vent_oscillation_period / spring_oscillation_period,
+    )
 
-    t = np.linspace(0, 10 * spring_oscillation_period, 1000)
+    t = np.linspace(0, vent_oscillation_period, 1000)
 
-    x = np.cos(vent_oscillation_period * t) + vent_radius * np.sin(vent_freq * t)
+    spring_y = -10 * np.cos(np.sqrt(spring_stiffness_koef / vent_weigth) * t)
 
-    plt.plot(t, x)
-    plt.plot(t, -x)
+    rx = vent_radius * np.cos(((2 * np.pi) / (vent_oscillation_period)) * t)
+    ry = vent_radius * np.sin(((2 * np.pi) / (vent_oscillation_period)) * t)
+
+    ry += spring_y
+
+    # plt.plot(t, spring_x)
+    plt.plot(rx, ry)
+    plt.ylabel("Проекция радиус вектора ry")
+    plt.xlabel("Проекция радиус вектора rx")
     plt.show()
