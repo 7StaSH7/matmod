4817d6cd1250f0a159aa1b545a05893a2ff5adfd
Aleksandr <36007931+7StaSH7@users.noreply.github.com>
2022-10-27 23:04:39 +0300

    lab4/done 100%

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -2,20 +2,14 @@
 import math
 import numpy as np
 
+spring_stiffness_koef = 0.9
 
-def main() -> None:
-    spring_stiffness_koef = 0.9
+vent_weigth = 1
+vent_radius = 5
 
-    vent_weigth = 1
-    vent_radius = 5
 
-    spring_oscillation_period = 2 * np.pi * np.sqrt(vent_weigth / spring_stiffness_koef)
-    vent_oscillation_period = 5 * spring_oscillation_period
-
-    print(
-        "Отношение периода вращения вентилятора к периоду колебания пружины:",
-        vent_oscillation_period / spring_oscillation_period,
-    )
+def calc(koef: int, spring_oscillation_period: float) -> None:
+    vent_oscillation_period = koef * spring_oscillation_period
 
     t = np.linspace(0, vent_oscillation_period, 1000)
 
@@ -25,9 +19,16 @@ def main() -> None:
     ry = vent_radius * np.sin(((2 * np.pi) / (vent_oscillation_period)) * t)
 
     ry += spring_y
-
-    # plt.plot(t, spring_x)
+    plt.figure()
+    plt.title(f"Соотношение = {vent_oscillation_period / spring_oscillation_period}")
     plt.plot(rx, ry)
     plt.ylabel("Проекция радиус вектора ry")
     plt.xlabel("Проекция радиус вектора rx")
+
+
+def main() -> None:
+    spring_oscillation_period = 2 * np.pi * np.sqrt(vent_weigth / spring_stiffness_koef)
+
+    for koef in range(1, 11):
+        calc(koef, spring_oscillation_period)
     plt.show()
