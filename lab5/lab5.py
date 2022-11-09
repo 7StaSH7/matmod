59d70679e3340253cd781fec72928f7a5de3bcdb
Aleksandr <36007931+7StaSH7@users.noreply.github.com>
2022-11-09 16:30:59 +0300

    lab5/fix

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,22 +3,22 @@ import numpy as np
 from scipy.integrate import odeint
 
 pray_birth = 1.
-pray_death_due_to_predators = 0.1
 predator_death = 1.5
-predator_groth = 0.75
+pray_death_due_to_predators = 0.1
+predator_growth = 0.75
 
 
 def calc_populate(population: np.array, t: np.linspace = 0) -> np.array:
-       return np.array([ pray_birth*population[0] - pray_death_due_to_predators*population[0]*population[1],
-                        -predator_death*population[1] + pray_death_due_to_predators*predator_groth*population[0]*population[1] ])
+       return np.array([(pray_birth - pray_death_due_to_predators*population[1]) * population[0],
+                        (-predator_death + pray_death_due_to_predators*predator_growth*population[0]) * population[1]])
     
 def main() -> None:
-    N0 = predator_death / predator_groth
-    M0 = pray_birth / pray_death_due_to_predators
+    N0 = pray_birth / pray_death_due_to_predators
+    M0 = predator_death / predator_growth
     print("N0", N0)
     print("M0", M0)
     # print("T", 8*np.pi/(M0 / N0))  !!! НАЙТИ !!!
-    initianal = np.array([M0, N0])
+    initianal = np.array([N0, M0])
     t = np.linspace(0, 17, 1000)
     # ode - ordinary differential equation или обыкновенное дифференциальное уравнение
     population, infodict = odeint(calc_populate, initianal, t, full_output=True) 
